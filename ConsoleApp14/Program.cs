b765309a48206b32932bf6180f704000c7847c56
_ArcLight_ <kakabadzerobert@gmail.com>
2024-04-02 13:38:06 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Введите колличество элементов в массиве:");
+
+        int elementsCount= int.Parse(Console.ReadLine());
+
+        int[] MyArray = new int[elementsCount];
+
+
+
+        for (int k = 0; k < MyArray.Length; k++  )
+        {
+            Console.WriteLine($"Введите элемент масива под индексом {k}:\t"); 
+            MyArray[k] = int.Parse(Console.ReadLine());
+        }
+
+
+        Console.WriteLine("Вывод массива");
+
+        for (int i = 0; i < MyArray.Length; i++) 
+        {
+            Console.WriteLine(MyArray[i]);
+        } 
+        Console.ReadLine();
+    }
+}
